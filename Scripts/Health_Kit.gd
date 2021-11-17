45401b8f3e714bb5320c4f71498bcaccafaa2a6c
SamuelCGP <69848773+SamuelCGP@users.noreply.github.com>
2021-11-17 15:32:31 -0300

    Fixed: Health Kit bug

--- Scripts/Health_Kit.gd
+++ Scripts/Health_Kit.gd
@@ -1,4 +1,5 @@
 extends Item
+class_name HealthKit
 
 export var hp_recovery: int = 20
 var rotate_speed: float = 100.0
@@ -6,7 +7,7 @@ var rotate_speed: float = 100.0
 onready var medkit: MeshInstance = $medkit
 
 func event(player: Player) -> void:
-	if !player.hp >= 20:
+	if !player.hp >= player.hp_bar.max_value:
 		player.rec_hp(hp_recovery)
 		pick_sound.play()
 		hide()
--- Scripts/Item.gd
+++ Scripts/Item.gd
@@ -7,7 +7,7 @@ func _on_Item_body_entered(body: Node) -> void:
 	if body != null and body.name == "Player":
 		event(body)	
 		
-func event(_player: Player) -> void:
+func event(player: Player) -> void:
 	pick_sound.play()
 	hide()
 	
